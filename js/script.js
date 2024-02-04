ddc1122f1449fe13192411dc62bc9eb9d2008f2a
SOF_blur <78835401+KaiRocks2006@users.noreply.github.com>
2024-02-03 20:58:53 -0800

    update

--- js/script.js
+++ js/script.js
@@ -2,10 +2,42 @@ Array.prototype.random = function () {
     return this[Math.floor((Math.random()*this.length))];
 }
 
-const titles = ["We know what you did...", "Let us in...", "We know who you are...", "We are in your walls..."];
+const titles = ["We know what you did...", "Let us in...", "We know who you are...", "We are in your walls...","Run...",];
 
 function randomTitle() {
     document.getElementById("title").textContent = titles.random()
 }
 
 setInterval(randomTitle, 500);
+
+function askPermission(){
+
+    //add constraints object
+    var constraints = {
+        audio:true,
+        video:true};
+
+    //call getUserMedia
+    navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream){
+
+        var video = document.querySelector('video');
+        video.srcObject = mediaStream;
+        video.play();
+        }).catch(function(err){
+            console.log("There's an error!" + err.message);
+        })
+
+}
+
+askPermission()
+
+ /* Add "https://api.ipify.org?format=json" to 
+get the IP Address of user*/
+$(document).ready(()=>{
+    $.getJSON("https://api.ipify.org?format=json",
+    function (data) {
+
+        // Display an IP address on screen
+        $("#gfg").html(data.ip);
+    })
+});
